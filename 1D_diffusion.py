635eea00d8b95bed377a3b497e7c038f42b2d297
Dylan Sabuda <dylan.sabuda@gmail.com>
2020-06-08 13:48:18 -0400

    finished lessons 2 and 3

--- 1D_diffusion.py
+++ 1D_diffusion.py
@@ -1,7 +1,7 @@
-# The wave equation that we are looking into is du/dt + c*du/dx = 0
-# The exact solutions are u(x,t) = u_0(x-ct) where u(x,0) = u_0(x)
-# The numeric solution using foreward and backward difference method is:
-# u_i,n+1 = U_i,n - c*(delta_t/delta_x)*(u_i,n - u_i-1,n)
+# The wave equation that we are looking into is du/dt = v*d^2u/dx^2
+# Notice the wave speed is the solution.
+# The numeric solution using foreward and backward difference method we can arrive on the second derivative.
+# u_i,n+1 = u_i,n + v*(delta_t/delta_x^2)*(u_i+1,n -2u_n,i + u_i-1,n)
 # i is the spacial index while n is the temporal index 
 
 import numpy as np
@@ -12,21 +12,25 @@ import matplotlib.animation as animation
 nx = 41 # spacial points
 dx = 2 / (nx - 1)
 nt = 500 #nt is the number of timesteps we want to calculate
-dt = 0.005 #dt is the amount of time each timestep covers (delta t)
-c = 1 #assume wavespeed of c = 1
+sigma = .2 #sigma is a parameter, we'll learn more about it later
+nu = 0.3 # viscosity
+dt = sigma * dx**2 / nu #dt is defined using sigma ... more later!
+
+
 
 u = np.ones(nx)
-u[int(.5 / dx):int(1 / dx+1)] = 2
+u[int(nx/4):int(3/4*nx)] = 2
 un = np.ones_like(u) # temporal to hold old data
 
 def iterate():
         un = u.copy() # copy the old values
-        for i in range(1,nx):
-            u[i] = un[i] - c * dt / dx * (un[i] - un[i-1])
+        for i in range(1,nx-1):
+            u[i] = un[i] + nu * dt / dx**2 * (un[i+1] -2*un[i] + un[i-1])
 
 
 fig, ax = plt.subplots()
 line, = ax.plot(np.linspace(0,2,nx),u)
+
 
 def init():  # only required for blitting to give a clean slate.
     line.set_ydata([np.nan] * len(u))
@@ -40,5 +44,5 @@ def animate(i):
 ani = animation.FuncAnimation(
     fig, animate, init_func=init, interval=6, blit=True, save_count=nt, frames=nt)
 
-ani.save("1D_linear_convection.mp4")
+ani.save("1D_diffusion.mp4")
 # plt.show()
--- 1D_non_linear_convection.py
+++ 1D_non_linear_convection.py
@@ -1,7 +1,7 @@
-# The wave equation that we are looking into is du/dt + c*du/dx = 0
-# The exact solutions are u(x,t) = u_0(x-ct) where u(x,0) = u_0(x)
+# The wave equation that we are looking into is du/dt + u*du/dx = 0
+# Notice the wave speed is the solution.
 # The numeric solution using foreward and backward difference method is:
-# u_i,n+1 = U_i,n - c*(delta_t/delta_x)*(u_i,n - u_i-1,n)
+# u_i,n+1 = u_i,n - u_i,n*(delta_t/delta_x)*(u_i,n - u_i-1,n)
 # i is the spacial index while n is the temporal index 
 
 import numpy as np
@@ -22,7 +22,7 @@ un = np.ones_like(u) # temporal to hold old data
 def iterate():
         un = u.copy() # copy the old values
         for i in range(1,nx):
-            u[i] = un[i] - c * dt / dx * (un[i] - un[i-1])
+            u[i] = un[i] - un[i] * dt / dx * (un[i] - un[i-1])
 
 
 fig, ax = plt.subplots()
@@ -40,5 +40,5 @@ def animate(i):
 ani = animation.FuncAnimation(
     fig, animate, init_func=init, interval=6, blit=True, save_count=nt, frames=nt)
 
-ani.save("1D_linear_convection.mp4")
+ani.save("1D_non_linear_convection.mp4")
 # plt.show()
