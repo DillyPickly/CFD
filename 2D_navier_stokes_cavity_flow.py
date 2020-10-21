0b42ed9a0c1a6cd560e6545ad87d6d356240451b
Dylan Sabuda <dylan.sabuda@gmail.com>
2020-10-21 01:42:34 -0400

    adding a navier stokes cavity flow

--- 2D_navier_stokes_cavity_flow.py
+++ 2D_navier_stokes_cavity_flow.py
@@ -5,17 +5,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib import pyplot, cm
 # 3D support
 from mpl_toolkits.mplot3d import Axes3D
 
-nx = 41 # spacial points
-ny = 41
-nt = 200 #nt is the number of timesteps we want to calculate
+nx = 81 # spacial points
+ny = 81
+nt = 800 #nt is the number of timesteps we want to calculate
+nit = 50
 dx = 2 / (nx - 1)
 dy = 2 / (ny - 1)
-sigma = 0.0099
-nu = 0.01
-dt = sigma * dx * dy / nu #dt is the amount of time each timestep covers (delta t)
+rho = 1
+nu = 0.1
+dt = .001 #dt is the amount of time each timestep covers (delta t)
 
 
 # Initial Conditions
@@ -27,6 +29,8 @@ vn = np.zeros_like(u) # temporal to hold old data
 
 p = np.zeros((nx, ny))
 pn = np.zeros_like(u) # temporal to hold old data
+
+b = np.zeros((nx, ny))
 
 # Boundary Conditions
 u[0,:]  = 0
@@ -49,66 +53,96 @@ xx, yy = np.meshgrid(x, y)
 def iterate():
         un = u.copy() # copy the old values
         vn = v.copy()
+        pn = p.copy()
 
         # Vectorized | most of the time is still wasted plotting
-        u[1:-1, 1:-1] = (un[1:-1, 1:-1] 
-                        - un[1:-1, 1:-1] * dt/dx * (un[1:-1, 1:-1] - un[:-2,1:-1]) 
-                        - vn[1:-1, 1:-1] * dt/dy * (un[1:-1, 1:-1] - un[1:-1, :-2]) 
-                        + nu*dt/dx**2*(un[2:,1:-1] - 2*un[1:-1, 1:-1] + un[:-2,1:-1]) 
-                        + nu*dt/dy**2 * (un[1:-1,2:] - 2*un[1:-1,1:-1] + un[1:-1,:-2]))
+        u[1:-1, 1:-1] = (un[1:-1, 1:-1]-
+                        un[1:-1, 1:-1] * dt / dx *
+                    (un[1:-1, 1:-1] - un[1:-1, 0:-2]) -
+                        vn[1:-1, 1:-1] * dt / dy *
+                    (un[1:-1, 1:-1] - un[0:-2, 1:-1]) -
+                        dt / (2 * rho * dx) * (p[1:-1, 2:] - p[1:-1, 0:-2]) +
+                        nu * (dt / dx**2 *
+                    (un[1:-1, 2:] - 2 * un[1:-1, 1:-1] + un[1:-1, 0:-2]) +
+                        dt / dy**2 *
+                    (un[2:, 1:-1] - 2 * un[1:-1, 1:-1] + un[0:-2, 1:-1])))
         
-        v[1:-1, 1:-1] = (vn[1:-1, 1:-1] 
-                        - vn[1:-1, 1:-1] * dt/dx * (vn[1:-1, 1:-1] - vn[:-2,1:-1]) 
-                        - un[1:-1, 1:-1] * dt/dy * (vn[1:-1, 1:-1] - vn[1:-1, :-2])
-                        + nu*dt/dx**2*(vn[2:,1:-1] - 2*vn[1:-1, 1:-1] + vn[:-2,1:-1]) 
-                        + nu*dt/dy**2 * (vn[1:-1,2:] - 2*vn[1:-1,1:-1] + vn[1:-1,:-2]))
+        v[1:-1,1:-1] = (vn[1:-1, 1:-1] -
+                un[1:-1, 1:-1] * dt / dx *
+                (vn[1:-1, 1:-1] - vn[1:-1, 0:-2]) -
+                vn[1:-1, 1:-1] * dt / dy *
+                (vn[1:-1, 1:-1] - vn[0:-2, 1:-1]) -
+                dt / (2 * rho * dy) * (p[2:, 1:-1] - p[0:-2, 1:-1]) +
+                nu * (dt / dx**2 *
+                (vn[1:-1, 2:] - 2 * vn[1:-1, 1:-1] + vn[1:-1, 0:-2]) +
+                dt / dy**2 *
+                (vn[2:, 1:-1] - 2 * vn[1:-1, 1:-1] + vn[0:-2, 1:-1])))
+
+        b[1:-1, 1:-1] = (rho * (1 / dt * 
+                    ((u[1:-1, 2:] - u[1:-1, 0:-2]) / 
+                     (2 * dx) + (v[2:, 1:-1] - v[0:-2, 1:-1]) / (2 * dy)) -
+                    ((u[1:-1, 2:] - u[1:-1, 0:-2]) / (2 * dx))**2 -
+                      2 * ((u[2:, 1:-1] - u[0:-2, 1:-1]) / (2 * dy) *
+                           (v[1:-1, 2:] - v[1:-1, 0:-2]) / (2 * dx))-
+                          ((v[2:, 1:-1] - v[0:-2, 1:-1]) / (2 * dy))**2))
+        
+        for q in range(nit):
+            pn = p.copy()
+            p[1:-1, 1:-1] = (((pn[1:-1, 2:] + pn[1:-1, 0:-2]) * dy**2 + 
+                            (pn[2:, 1:-1] + pn[0:-2, 1:-1]) * dx**2) /
+                            (2 * (dx**2 + dy**2)) -
+                            dx**2 * dy**2 / (2 * (dx**2 + dy**2)) * 
+                            b[1:-1,1:-1])
 
         # Boundary Conditions
-        u[0,:]  = 0
-        u[-1,:] = 0
-        u[:,0]  = 0
-        u[:,-1] = 1
+        u[0, :]  = 0
+        u[:, 0]  = 0
+        u[:, -1] = 0
+        u[-1, :] = 1    # set velocity on cavity lid equal to 1
+        v[0, :]  = 0
+        v[-1, :] = 0
+        v[:, 0]  = 0
+        v[:, -1] = 0
 
-        v[0,:]  = 0
-        v[-1,:] = 0
-        v[:,0]  = 0
-        v[:,-1] = 0
+        p[:, -1] = p[:, -2] # dp/dx = 0 at x = 2
+        p[0, :] = p[1, :]   # dp/dy = 0 at y = 0
+        p[:, 0] = p[:, 1]   # dp/dx = 0 at x = 0
+        p[-1, :] = 0        # p = 0 at y = 2
 
-        p[0,:]  = p[1,:]
-        p[-1,:] = p[-2,:]
-        p[:,0]  = p[:,1]
-        p[:,-1] = 0
+        return u,v,p
+
+# for i in range(nt):
+#     iterate()
 
 
 # set up the figure and axis
-fig = plt.figure(figsize=(12,6))
-ax1 = fig.add_subplot(121, projection='3d')
-ax2 = fig.add_subplot(122, projection='3d') 
-surface = [ax1.plot_surface(xx, yy, u, color='0.75', rstride=1, cstride=1),
-            ax2.plot_surface(xx, yy, v, color='0.75', rstride=1, cstride=1)]
-
+fig = plt.figure(figsize=(6,6))
+ax1 = fig.add_subplot(111)
+ax1.contourf(xx, yy, p, alpha=0.7) #, cmap=cm.viridis)
+ax1.contour(xx, yy, p) #, cmap=cm.viridis)  
+ax1.quiver(xx[::2, ::2], yy[::2, ::2], u[::2, ::2], v[::2, ::2])
+surface = [ax1]
 # set axis parameters
 ax1.set_xlim([0,2])
 ax1.set_ylim([0,2])
-ax1.set_zlim([1,2])
 
-ax2.set_xlim([0,2])
-ax2.set_ylim([0,2])
-ax2.set_zlim([1,2])
-
-ax1.set_title(r'$u$(x,y,T)')
-ax2.set_title(r'$v$(x,y,T)')
+ax1.set_title(r'')
 
 def animate(i, surface):
     iterate()
     ax1.collections.clear()
-    ax2.collections.clear()
-    surface[0] = ax1.plot_surface(xx, yy, u, cmap="magma",edgecolor='none')
-    surface[1] = ax2.plot_surface(xx, yy, v, cmap="magma",edgecolor='none')
-
+    ax1.contourf(xx, yy, p, alpha=0.7) #, cmap=cm.viridis)
+    ax1.contour(xx, yy, p) #, cmap=cm.viridis)  
+    ax1.quiver(xx[::2, ::2], yy[::2, ::2], u[::2, ::2], v[::2, ::2])
+    surface = [ax1]
+    return surface
+    
 # init_func=init, blit=True
 ani = animation.FuncAnimation(
     fig, animate, interval=1000/25, save_count=nt, frames=nt, fargs=([surface]))
 
-ani.save("2D_burgers.mp4",dpi=600)
+ani.save("2D_navier_stokes_cavity_flow.mp4",dpi=600)
+# fig.colorbar(cm.ScalarMappable(norm=None, cmap=None))
+# for ii in v:
+#     print(ii)
 # plt.show() # I need to change nx and ny to 21 to get the plots to render realtime smoothly
